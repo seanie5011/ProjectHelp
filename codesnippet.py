0f310ac6102b5822988973f07927ae417ea5cffe
seanie5011 <seanie500@hotmail.com>
2023-05-05 13:41:05 +0100

    Initialisation and adding pygame template.

---
+++ codesnippet.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# FUNCTIONS
+
+def add_two_arrays(a, b):
+	'''
+	Function to add two arrays, returning: a + b
+	'''
+
+	# return the addition of a and b
+	return a + b
+
+# MAIN LOOP
+
+if __name__ == '__main__':
+	# define two arrays
+	a = np.array([1, 2, 3])
+	a = np.array([1, 2, 3])
+
+	# add them and print the result
+	added = add_two_arrays(a, b)
+	print(added)
